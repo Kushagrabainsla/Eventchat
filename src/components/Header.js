e4a03ceecb150d09e1207d2b4e128124deda373a
Kushagra Bainsla <kushagrabainsla@gmail.com>
2021-02-12 20:37:17 +0530

    Home

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+function Header() {
+    return (
+        <div className="header">
+            <img className="header__logo1" src="assets/app__logo.png" alt="" />
+            <img className="header__logo1" src="assets/app__logo.png" alt="" />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div className='container'>
+            <div className='home'>
+                <img className="send__logo" src="assets/send_button.png" alt="" />
+            </div>
+        </div>
+
+    );
+}
+
+export default Home
